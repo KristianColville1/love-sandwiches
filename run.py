30c1febb206c18739cc6be47dd737a561f1d47cc
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-03-25 13:54:44 +0000

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 """Love Sandwiches Walk Through Project with Code Institute for Portfolio Project 3"""
 
+from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -88,6 +90,20 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet('sales')
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -99,4 +115,5 @@ def main():
     update_worksheet(new_surplus_data, 'surplus')
 
 print('Welcome to Love Sandwiches Data Automation')
-main()
+# main()
+sales_columns = get_last_5_entries_sales()
